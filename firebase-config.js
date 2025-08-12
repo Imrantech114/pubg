c165fb6c965cbba830d550700b7606cdd677ded0
Imrantech114 <74811477+Imrantech114@users.noreply.github.com>
2025-08-13 02:52:35 +0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // Replace the placeholders with your Firebase config keys
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR-MESSAGING-SENDER-ID",
-  appId: "YOUR-APP-ID"
+  apiKey: "AIzaSyD3dsVqro1U5UzrX_FyD907PnjzOG08lPA",
+  authDomain: "pubg-tournament-4aac7.firebaseapp.com",
+  projectId: "pubg-tournament-4aac7",
+  storageBucket: "pubg-tournament-4aac7.firebasestorage.app",
+  messagingSenderId: "1073527020666",
+  appId: "1:1073527020666:web:c09349425df05e9f1eafc3"
 };
-
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
